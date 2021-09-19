9c590115b5467df1555b64cd5cb4b24f717bb045
Anton <anton.valt@gmail.co>
2021-09-19 16:46:15 +0300

    add modal, add card's classes (2)

--- js/script.js
+++ js/script.js
@@ -99,12 +99,15 @@ window.addEventListener('DOMContentLoaded', () => {
             modalAlert = document.querySelector('.modal'),
             modalClose = document.querySelector('[data-close]')
 
+      function showModal() {
+            modalAlert.classList.add('show')
+            modalAlert.classList.remove('hide')
+            document.body.style.overflow = 'hidden'
+            clearInterval(modalTimerId)
+      }
+
       modalTrigger.forEach(btn => {
-            btn.addEventListener('click', () => {
-                  modalAlert.classList.add('show')
-                  modalAlert.classList.remove('hide')
-                  document.body.style.overflow = 'hidden'
-            })
+            btn.addEventListener('click', showModal)
       })
 
       function closeModal() {
@@ -126,5 +129,78 @@ window.addEventListener('DOMContentLoaded', () => {
                   closeModal()
             }
       })
+
+      const modalTimerId = setTimeout(showModal, 10000)
+
+      function showModalByScroll() {
+            if (window.pageYOffset + document.documentElement.clientHeight >= document.
+                  documentElement.scrollHeight) {
+                        showModal()
+                        window.removeEventListener('scroll', showModalByScroll)
+            }
+      }
+
+      window.addEventListener('scroll', showModalByScroll)
+
+      //Use Card's Classes
       
-});
+      class MenuCard {
+            constructor(src, alt, title, descr, price, parentSelector) {
+                  this.src = src
+                  this.alt = alt
+                  this.title = title
+                  this.descr = descr
+                  this.price = price
+                  this.parent = document.querySelector(parentSelector)
+                  this.transfer = 27
+                  this.changeToUAH()
+            }
+
+            changeToUAH() {
+                  this.price = this.price * this.transfer
+            }
+
+            render() {
+                 const element = document.createElement('div') 
+                 element.innerHTML = `
+                        <div class="menu__item">
+                              <img src=${this.src} alt=${this.alt}>
+                              <h3 class="menu__item-subtitle">${this.title}</h3>
+                              <div class="menu__item-descr">${this.descr}</div>
+                              <div class="menu__item-divider"></div>
+                              <div class="menu__item-price">
+                              <div class="menu__item-cost">Цена:</div>
+                              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                              </div>
+                        </div>
+                 `
+                 this.parent.append(element)
+            }
+      }
+      new MenuCard(
+            "img/tabs/vegy.jpg",
+            "vegy",
+            'Меню "Фитнес"',
+            'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+            9,
+            ".menu .container"
+      ).render()
+
+      new MenuCard(
+            "img/tabs/elite.jpg",
+            "elite",
+            'Меню “Премиум”',
+            'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+            14,
+            ".menu .container"
+      ).render()
+
+      new MenuCard(
+            "img/tabs/post.jpg",
+            "post",
+            'Меню "Постное"',
+            'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+            21,
+            ".menu .container"
+      ).render()
+})
